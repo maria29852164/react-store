bef641b28f410fef5c6f59450192b0ae8e2e7b64
maria de los angeles <mariaramireeez2002@gmail.com>
2022-07-21 19:22:12 -0400

    configuracion de react

---
+++ src/components/app.jsx
@@ -0,0 +1,8 @@
+
+import React from 'react'
+import './app.css'
+export const App= ()=> {
+  return (
+     <div>componente app</div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MinCssExtractPlugin= require('mini-css-extract-plugin')
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+    entry: './src/index.js',
+    output:{
+        path:path.resolve(__dirname, 'dist'),
+        filename:'bundle.js'
+    },
+    mode:'development',
+    resolve:{
+        extensions:['.js','.jsx']
+    },
+    module:{
+        rules:[
+            {
+                test:/\.(js|jsx)$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:'babel-loader'
+                }
+            },
+            {
+                test:/\.html$/,
+                use:[
+                    {
+                        loader:'html-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {loader: MinCssExtractPlugin.loader},
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./public/index.html',
+            filename:'./index.html'
+        }),
+        new MinCssExtractPlugin({
+            filename: 'assets/[name].css'
+        })
+    ],
+    devServer:{
+        allowedHosts:path.join(__dirname,'dist'),
+        compress:true,
+        port:3005
+    }
+}
